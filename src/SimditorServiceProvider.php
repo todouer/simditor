7b60df537208adbebdf1889ba1be86ccfa036cca
tudouer <73217797+todouer@users.noreply.github.com>
2025-11-03 10:45:20 +0800

    Support php-casbin Laravel Admin

--- src/SimditorServiceProvider.php
+++ src/SimditorServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace Jxlwqq\Simditor;
 
-use Encore\Admin\Admin;
-use Encore\Admin\Form;
 use Illuminate\Support\ServiceProvider;
+use RuntimeException;
 
 class SimditorServiceProvider extends ServiceProvider
 {
@@ -28,8 +27,49 @@ class SimditorServiceProvider extends ServiceProvider
             );
         }
 
-        Admin::booting(function () {
-            Form::extend('simditor', Editor::class);
+        $this->resolveAdminClass()::booting(function () {
+            $this->resolveFormClass()::extend('simditor', Editor::class);
         });
+    }
+
+    /**
+     * Resolve the Laravel-admin Admin facade class for the current installation.
+     */
+    protected function resolveAdminClass(): string
+    {
+        return $this->resolveAdminComponent('Admin');
+    }
+
+    /**
+     * Resolve the Laravel-admin Form class for the current installation.
+     */
+    protected function resolveFormClass(): string
+    {
+        return $this->resolveAdminComponent('Form');
+    }
+
+    /**
+     * Resolve Laravel-admin classes for both the legacy encore/laravel-admin package and the
+     * php-casbin/laravel-admin fork that targets newer Laravel versions.
+     *
+     * @throws RuntimeException
+     */
+    protected function resolveAdminComponent(string $component): string
+    {
+        $namespaces = [
+            'Encore\\Admin',
+            'LaravelAdmin',
+            'PhpCasbin\\LaravelAdmin',
+        ];
+
+        foreach ($namespaces as $namespace) {
+            $class = sprintf('%s\\%s', $namespace, $component);
+
+            if (class_exists($class)) {
+                return $class;
+            }
+        }
+
+        throw new RuntimeException(sprintf('Unable to locate the Laravel-admin %s class.', $component));
     }
 }
